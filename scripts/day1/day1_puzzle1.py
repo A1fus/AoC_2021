9d7687ecc18d9375a70130299d579c19a76d87e2
Alfie Bowman <alfiebowman@protonmail.com>
2021-12-01 11:32:13 +0000

    Pass puzzle 2

--- scripts/day1/day1_puzzle1.py
+++ scripts/day1/day1_puzzle1.py
@@ -1,5 +1,7 @@
-"""count the number of times a depth measurement increases from the previous measurement."""
 import pandas as pd
+
+
+"""count the number of times a depth measurement increases from the previous measurement."""
 
 data = pd.read_csv("input/day1.csv", header=None, usecols=[0])
 data_diff = data.diff()
